8919aab7bd7f7edd7ab1e7933482ca92f1887d95
ouli10 <34356682+ouli10@users.noreply.github.com>
2019-10-21 11:10:18 +0200

    Add files via upload

--- src/API/Migrations/Migrations20191018.php
+++ src/API/Migrations/Migrations20191018.php
@@ -16,5 +16,13 @@ class Migrations20191018 extends AbstractMigrations
 			name VARCHAR(100),
 			image VARCHAR(100)
 		);
+		CREATE TABLE destination.country(
+			id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+			name VARCHAR(100),
+			city_id TINYINT UNSIGNED NOT NULL,
+			CONSTRAINT fk_city_country FOREIGN KEY (city_id) REFERENCES city(id)
+
+		);
+
 	";
 }
--- src/API/Migrations/Migrations20191019.php
+++ src/API/Migrations/Migrations20191019.php
@@ -11,9 +11,15 @@ class Migrations20191019 extends AbstractMigrations
 	protected $sql = "
 		INSERT INTO destination.city
 		VALUES
-			(NULL, 'paris', 'paris.jpg'),
-			(NULL, 'tokyo', 'tokyo.jpg'),
-			(NULL, 'londres', 'londres.jpg')
+			(NULL, 'Paris', 'https://ssl.tzoo-img.com/images/tzoo.99175.0.797942.paris.jpg?width=392&v=2&spr=3'),
+			(NULL, 'Tokyo', 'https://www.partir.com/images/incontournables/japon-tokyo-shinjuku.jpg'),
+			(NULL, 'Londres', 'https://voyageursintrepides.com/wp-content/uploads/2018/07/que-faire-soir-nuit-londres-sortir.jpg')
+		;
+		INSERT INTO destination.country
+		VALUES
+			(NULL, 'France', '1'),
+			(NULL, 'Japan', '2'),
+			(NULL, 'Royaume-Uni', '3')
 		;
 	";
 }
